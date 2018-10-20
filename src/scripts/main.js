ce888791ebb52a280e7e0440ab755f0bf4fd8e31
Campbell Phalen <cphalen@ucls.uchicago.edu>
2018-10-19 18:35:12 -0600

    Final early changes

---
+++ src/scripts/main.js
@@ -0,0 +1,23 @@
+states = ["Projects", "Activities", "Work"];
+ACTIVE_CLASSNAME = "nav-link active";
+INACTIVE_CLASSNAME = "nav-link";
+
+function setState(state) {
+    for(var i = 0; i < states.length; i++) {
+        stateElement = document.getElementsByClassName(states[i])[0];
+        buttonClassName = states[i] + "-button";
+        button = document.getElementsByClassName(buttonClassName)[0];
+        console.log(buttonClassName);
+        console.log(states[i] + " != " + state);
+        if(states[i] == state) {
+            stateElement.style.display = "block";
+            button.className = ACTIVE_CLASSNAME + " " + buttonClassName;
+        } else {
+            stateElement.style.display = "none";
+            button.className = INACTIVE_CLASSNAME + " " + buttonClassName;
+        }
+    }
+}
+
+// Start with projects visible
+setState("Projects");
