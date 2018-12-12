cad69b7ad5b98e4a662077d19046459e3c28da17
Campbell Phalen <cphalen@ucls.uchicago.edu>
2018-12-12 16:21:31 -0600

    Initial commit for Shakespeare game -- files need to be hosted someone online for AJAX get

---
+++ src/scripts/shakespeare.js
@@ -0,0 +1,16 @@
+/*
+
+All plays courtesy of 'shakespeare.mit.edu'
+we thank them for democratizing this
+literature that has changed the world!
+
+*/
+
+$.ajax({
+    url:'http://cphalen.github.io/Shakespeare/plays/Hamlet_Entire Play.html',
+        type:'GET',
+        success: function(data){
+            console.log(data);
+           // $('#content').html($(data).find('#firstHeading').html());
+        }
+});
