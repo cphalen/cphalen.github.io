89568eeddaf56e4f4881135de3df31b90500c0b8
Campbell Phalen <cphalen@ucls.uchicago.edu>
2018-12-12 18:16:28 -0600

    About halfway through project -- midway check

--- src/scripts/shakespeare.js
+++ src/scripts/shakespeare.js
@@ -7,10 +7,116 @@ literature that has changed the world!
 */
 
 $.ajax({
-    url:'https://cphalen.github.io/Shakespeare/plays/Hamlet_ Entire Play.html',
+    url:'https://cphalen.github.io/Shakespeare/plays/The Tempest_ Entire Play.html',
         type:'GET',
         success: function(data){
-            console.log(data);
-           // $('#content').html($(data).find('#firstHeading').html());
+            main(data);
         }
 });
+
+
+// We will continue to pull lines from content, so we
+// keep it global to ensure that it persists
+content = {}
+
+function main(text) {
+    parser = new DOMParser();
+    play = parser.parseFromString(text,"text/xml");
+
+    // Get body of html page containing play data
+    body = play.getElementsByTagName("body")[0];
+
+
+    act = "";
+    scene = "";
+    pass = true;
+    currentSpeech = 0
+    for(var i = 0; i < body.children.length; i++) {
+        // Go from blockquote -> a -> h3 because
+        // this is the order of frequency that the
+        // tags occur. This increases efficency for
+        // by having less checks that we know are
+        // going to be incorrect
+
+        current = body.children[i]
+        if(current.tagName == "BLOCKQUOTE" && pass == false) {
+            lines = current.getElementsByTagName("a");
+            content[currentSpeech]["BeginningLine"] = lines[0].name;
+            content[currentSpeech]["EndingLine"] = lines[lines.length - 1].name;
+            for(var j = 0; j < lines.length; j++) {
+                content[currentSpeech]["Text"] += lines[j].textContent + "\n";
+            }
+        } else if(current.tagName == "A"){
+            currentSpeech++;
+            content[currentSpeech] = {
+                "ACT": act,
+                "SCENE": scene,
+                "Speaker": current.textContent,
+                "BeginningLine": "",
+                "EndingLine": "",
+                "Text": "\n" //Initalize to start with an endline -- just removes some issues
+            }
+            pass = false;
+        } else if(current.tagName == "H3") {
+            if(current.textContent.includes("ACT")) {
+                // This works because the numeral 'I', 'II', 'IV', etc.
+                // Always comes directly after the first space
+                words = current.textContent.split(" ");
+                act = words[1];
+            } else if(current.textContent.includes("SCENE")) {
+                // This works because the numeral 'I', 'II', 'IV', etc.
+                // Always comes directly after the first space
+                words = current.textContent.split(" ");
+                scene = words[1];
+            }
+
+            // Pass the blockquote that comes after a new scene
+            // or a new act because these ones are normally just
+            // scene descriptions
+            pass = true;
+        }
+    }
+}
+
+function getRandomInt(max) {
+  return Math.floor(Math.random() * Math.floor(max));
+}
+
+function getDifficulty(line) {
+    numbers  = line["BeginningLine"].split(".");
+    start    = parseInt(numbers[numbers.length - 1]);
+    numbers  = line["EndingLine"].split(".");
+    end      = parseInt(numbers[numbers.length - 1]);
+    length   = end - start;
+
+    if(length < 3) {
+        return 3;
+    } else if (length < 8) {
+        return 2;
+    } else {
+        return 1;
+    }
+}
+
+function getLine(difficulty) {
+    // Continue searching until we get a line
+    // of the proper difficulty
+    while(true) {
+        max = Object.keys(content).length;
+        random = getRandomInt(max);
+        if (getDifficulty(content[random]) == difficulty) {
+            return content[random];
+        };
+    }
+}
+
+currentLine = null;
+
+function populateQuote(difficulty) {
+    line = getLine(difficulty);
+    // So that the solution persists
+    currentLine = line;
+
+    pre = $("pre#quoteSpace")[0];
+    pre.textContent = line["text"];
+}
